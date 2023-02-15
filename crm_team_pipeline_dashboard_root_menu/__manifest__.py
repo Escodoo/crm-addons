a4d268e951c23dd514332b96dc6bf68d0ee4c59a
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-02-15 16:16:59 -0300

    [FIX] crm_team_pipeline_dashboard_root_menu: pre-commit

--- crm_team_pipeline_dashboard_root_menu/__manifest__.py
+++ crm_team_pipeline_dashboard_root_menu/__manifest__.py
@@ -2,19 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'CRM Team Pipeline Dashboard Default Root Menu',
-    'summary': """
+    "name": "CRM Team Pipeline Dashboard Default Root Menu",
+    "summary": """
         This module changes the CRM root menu to open Team Pipeline Dashboard""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/crm',
-    'depends': [
-        'crm',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/escodoo/crm-addons",
+    "depends": [
+        "crm",
     ],
-    'data': [
-        'views/crm_lead_views.xml',
+    "data": [
+        "views/crm_lead_views.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
